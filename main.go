ea06642f72ff2c19cc75c0ba36e84453ab2039c3
dave@com1software.com <dave@com1software.com>
2023-05-04 15:12:47 -0400

    update

--- main.go
+++ main.go
@@ -25,7 +25,6 @@ func main() {
 		fmt.Println("Server running....")
 		fmt.Println("Listening on port 8080")
 		fmt.Println("")
-		//------------------------------------------------ Init Page Handler
 		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			xdata := InitPage()
 			fmt.Fprint(w, xdata)
@@ -45,6 +44,12 @@ func main() {
 			xdata := DisplayPage()
 			fmt.Fprint(w, xdata)
 		})
+		//------------------------------------------------ Test Page Handler
+		http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+			xdata := TestPage()
+			fmt.Fprint(w, xdata)
+		})
+
 		//------------------------------------------------- Static Handler Handler
 		fs := http.FileServer(http.Dir("static/"))
 		http.Handle("/static/", http.StripPrefix("/static/", fs))
@@ -107,9 +112,9 @@ func InitPage() string {
 	xdata = xdata + "  <A HREF='http://" + xip + ":8080/about'> [ About ] </A>  "
 	xdata = xdata + "  <A HREF='http://" + xip + ":8080/playground'> [ PlayGround ] </A>  "
 	xdata = xdata + "  <A HREF='http://" + xip + ":8080/display'> [ Display ] </A>  "
+	xdata = xdata + "  <A HREF='http://" + xip + ":8080/test'> [ Testing ] </A>  "
 	xdata = xdata + "  <A HREF='http://" + xip + ":8080/static/test123.html'> [ Test123 ] </A>  "
-
-	xdata = xdata + "<BR><BR>Go Web Server"
+	xdata = xdata + "<BR><BR>Go Web Server...."
 	//------------------------------------------------------------------------
 	xdata = xdata + " </body>"
 	xdata = xdata + " </html>"
@@ -146,10 +151,71 @@ func AboutPage() string {
 	//------------------------------------------------------------------------
 	xdata = xdata + "<body onload='startTime()'>"
 	xdata = xdata + "<p>Go Web Server</p>"
-	xdata = xdata + "<div id='txt'></div>"
+	xdata = xdata + "<div id='txtdt'></div>"
 	//---------
 	xdata = xdata + "Go Web Server"
 	//------------------------------------------------------------------------
+
+	//------------------------------------------------------------------------
+	xdata = xdata + " </body>"
+	xdata = xdata + " </html>"
+	return xdata
+
+}
+
+//----------------------------------------------------------------
+func TestPage() string {
+	//---------------------------------------------------------------------------
+
+	//----------------------------------------------------------------------------
+	xdata := "<!DOCTYPE html>"
+	xdata = xdata + "<html>"
+	xdata = xdata + "<head>"
+	//------------------------------------------------------------------------
+	xdata = xdata + "<title>Test Page</title>"
+	xdata = xdata + "  <link REL='StyleSheet' TYPE='text/css' HREF='static/css/style.css'>"
+	//------------------------------------------------------------------------
+	xdata = DateTimeDisplay(xdata)
+	xdata = xdata + "<style>"
+	xdata = xdata + "body {"
+	xdata = xdata + "    background-color: grey;"
+	xdata = xdata + "}"
+	xdata = xdata + "	h1 {"
+	xdata = xdata + "	color: white;"
+	xdata = xdata + "	text-align: center;"
+	xdata = xdata + "}"
+	xdata = xdata + "	p1 {"
+	xdata = xdata + "color: green;"
+	xdata = xdata + "font-family: verdana;"
+	xdata = xdata + "	font-size: 20px;"
+	xdata = xdata + "}"
+	xdata = xdata + "	p2 {"
+	xdata = xdata + "color: red;"
+	xdata = xdata + "font-family: verdana;"
+	xdata = xdata + "	font-size: 20px;"
+	xdata = xdata + "}"
+	xdata = xdata + "	div {"
+	xdata = xdata + "color: white;"
+	xdata = xdata + "font-family: verdana;"
+	xdata = xdata + "	font-size: 20px;"
+	xdata = xdata + "	text-align: center;"
+	xdata = xdata + "}"
+	xdata = xdata + "</style>"
+	xdata = xdata + "</head>"
+	//------------------------------------------------------------------------
+	xdata = xdata + "<body onload='startTime()'>"
+	xdata = xdata + "<H1>Test Page</H1>"
+	xdata = xdata + "<div id='txtdt'></div>"
+	//---------
+	xdata = xdata + "<center>"
+	xdata = xdata + "<p1>Go Web Server</p1>"
+	xdata = xdata + "<BR>"
+	xdata = xdata + "<p2>Go Web Server</p2>"
+	xdata = xdata + "</center>"
+
+	//------------------------------------------------------------------------
+	//	xdata = xdata + "  <A HREF='http://" + xip + ":8080'> [ Return to Start Page ] </A>  "
+
 	//------------------------------------------------------------------------
 	xdata = xdata + " </body>"
 	xdata = xdata + " </html>"
@@ -199,7 +265,7 @@ func DisplayPage() string {
 	//------------------------------------------------------------------------
 	xdata = xdata + "<body onload='startTime()'>"
 	xdata = xdata + "<H1>Display Page</H1>"
-	xdata = xdata + "<div id='txt'></div>"
+	xdata = xdata + "<div id='txtdt'></div>"
 	//---------
 	xdata = xdata + "<center>"
 	xdata = xdata + "<p1>Go Web Server</p1>"
@@ -208,6 +274,7 @@ func DisplayPage() string {
 	xdata = xdata + "</center>"
 
 	//------------------------------------------------------------------------
+
 	//------------------------------------------------------------------------
 	xdata = xdata + " </body>"
 	xdata = xdata + " </html>"
@@ -228,98 +295,7 @@ func PlayGroundPage() string {
 	xdata = xdata + "<title>The PlayGround</title>"
 	xdata = xdata + "  <link REL='StyleSheet' TYPE='text/css' HREF='static/css/style.css'>"
 	//------------------------------------------------------------------------
-	xdata = xdata + "<script>"
-	//------------------------------------------------------------------------
-	xdata = xdata + "function startTime() {"
-	xdata = xdata + "  var today = new Date();"
-	xdata = xdata + "  var d = today.getDay();"
-	xdata = xdata + "  var h = today.getHours();"
-	xdata = xdata + "  var m = today.getMinutes();"
-	xdata = xdata + "  var s = today.getSeconds();"
-	xdata = xdata + "  var ampm = h >= 12 ? 'pm' : 'am';"
-	xdata = xdata + "  var mo = today.getMonth();"
-	xdata = xdata + "  var dm = today.getDate();"
-	xdata = xdata + "  var yr = today.getFullYear();"
-	xdata = xdata + "  m = checkTimeMS(m);"
-	xdata = xdata + "  s = checkTimeMS(s);"
-	xdata = xdata + "  h = checkTimeH(h);"
-	//------------------------------------------------------------------------
-	xdata = xdata + "  switch (d) {"
-	xdata = xdata + "    case 0:"
-	xdata = xdata + "       day = 'Sunday';"
-	xdata = xdata + "    break;"
-	xdata = xdata + "    case 1:"
-	xdata = xdata + "    day = 'Monday';"
-	xdata = xdata + "        break;"
-	xdata = xdata + "    case 2:"
-	xdata = xdata + "        day = 'Tuesday';"
-	xdata = xdata + "        break;"
-	xdata = xdata + "    case 3:"
-	xdata = xdata + "        day = 'Wednesday';"
-	xdata = xdata + "        break;"
-	xdata = xdata + "    case 4:"
-	xdata = xdata + "        day = 'Thursday';"
-	xdata = xdata + "        break;"
-	xdata = xdata + "    case 5:"
-	xdata = xdata + "        day = 'Friday';"
-	xdata = xdata + "        break;"
-	xdata = xdata + "    case 6:"
-	xdata = xdata + "        day = 'Saturday';"
-	xdata = xdata + "}"
-	//------------------------------------------------------------------------------------
-	xdata = xdata + "  switch (mo) {"
-	xdata = xdata + "    case 0:"
-	xdata = xdata + "       month = 'January';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 1:"
-	xdata = xdata + "       month = 'Febuary';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 2:"
-	xdata = xdata + "       month = 'March';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 3:"
-	xdata = xdata + "       month = 'April';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 4:"
-	xdata = xdata + "       month = 'May';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 5:"
-	xdata = xdata + "       month = 'June';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 6:"
-	xdata = xdata + "       month = 'July';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 7:"
-	xdata = xdata + "       month = 'August';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 8:"
-	xdata = xdata + "       month = 'September';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 9:"
-	xdata = xdata + "       month = 'October';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 10:"
-	xdata = xdata + "       month = 'November';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "    case 11:"
-	xdata = xdata + "       month = 'December';"
-	xdata = xdata + "       break;"
-	xdata = xdata + "}"
-	//  -------------------------------------------------------------------
-	xdata = xdata + "  document.getElementById('txt').innerHTML = ' - '+h + ':' + m + ':' + s+' '+ampm+' - '+day+', '+month+' '+dm+', '+yr;"
-	xdata = xdata + "  var t = setTimeout(startTime, 500);"
-	xdata = xdata + "}"
-	//----------
-	xdata = xdata + "function checkTimeMS(i) {"
-	xdata = xdata + "  if (i < 10) {i = '0' + i};"
-	xdata = xdata + "  return i;"
-	xdata = xdata + "}"
-	//----------
-	xdata = xdata + "function checkTimeH(i) {"
-	xdata = xdata + "  if (i > 12) {i = i -12};"
-	xdata = xdata + "  return i;"
-	xdata = xdata + "}"
-	xdata = xdata + "</script>"
+	xdata = DateTimeDisplay(xdata)
 	//------------------------------------------------------------------------
 	xdata = xdata + "</head>"
 	//------------------------------------------------------------------------
@@ -330,8 +306,11 @@ func PlayGroundPage() string {
 	xdata = xdata + "<button onclick='myFunction1()'>Click me 1</button> - "
 	xdata = xdata + "<button onclick='myFunction2()'>Click me 2</button>"
 	xdata = xdata + "<p id='demo2'></p>"
+	xdata = xdata + "<div id='txtdt'></div>"
+
 	//---------
 	xdata = xdata + "Go Web Server"
+
 	//------------------------------------------------------------------------
 	xdata = xdata + "<script>"
 	//----------
@@ -432,7 +411,7 @@ func DateTimeDisplay(xdata string) string {
 	xdata = xdata + "       break;"
 	xdata = xdata + "}"
 	//  -------------------------------------------------------------------
-	xdata = xdata + "  document.getElementById('txt').innerHTML = ' - '+h + ':' + m + ':' + s+' '+ampm+' - '+day+', '+month+' '+dm+', '+yr;"
+	xdata = xdata + "  document.getElementById('txtdt').innerHTML = ' - '+h + ':' + m + ':' + s+' '+ampm+' - '+day+', '+month+' '+dm+', '+yr;"
 	xdata = xdata + "  var t = setTimeout(startTime, 500);"
 	xdata = xdata + "}"
 	//----------
